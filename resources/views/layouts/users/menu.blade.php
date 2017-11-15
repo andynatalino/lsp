81d6230c34a5aed7f36feca9a424a602cf9f3cae
andynl <andy.natalino@gmail.com>
2017-11-15 21:07:23 +0700

    updates

--- resources/views/layouts/users/menu.blade.php
+++ resources/views/layouts/users/menu.blade.php
@@ -10,24 +10,29 @@ $s = App\Setting::first();
         <li><a href="{{ url('berita') }}">Berita</a></li>
         <li><a href="{{ url('kontak') }}">Kontak</a></li>
         <li><a href="{{ url('galeri') }}">Galeri</a></li>
-        <li><a href="{{ url('tentang') }}">Tentang</a></li>
       </ul>
-
-      @if(Auth::check())
-      <div class="app-bar-element place-right">
-        <a class="dropdown-toggle fg-white"><span class="mif-user"></span> {{ Auth::user()->name }}</a>
+      <div class="app-bar-element">
+        <a class="dropdown-toggle fg-white">Tentang</a>
         <ul class="d-menu" data-role="dropdown">
-          <li><a href="{{ url('profil')}}"><span class="mif-user"></span> Profil</a></li>             
-          <li><a href="{{ url('pembayaran')}}"><span class="mif-money"></span> Pembayaran</a></li>
-          <li><a href="{{ url('profil/konfirmasi')}}"><span class="mif-user-check"></span> Konfirmasi</a></li>
-          <li><a href="{{ url('profil/transaksisaya')}}"><span class="mif-list"></span> Transaksi Saya</a></li>
-          <li><a href="{{ url('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><span class="mif-exit"></span> Logout</a></li>
-          <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-            {{ csrf_field() }}
-          </form>
+          <li><a href="{{ url('#')}}">Profil</a></li>             
+          <li><a href="{{ url('#')}}">Visi Misi</a></li>
+          <li><a href="{{ url('#')}}">Legalitas Perusahaan</a></li>        
         </div>
-      </ul>
+        @if(Auth::check())
+        <div class="app-bar-element place-right">
+          <a class="dropdown-toggle fg-white"><span class="mif-user"></span> {{ Auth::user()->name }}</a>
+          <ul class="d-menu" data-role="dropdown">
+            <li><a href="{{ url('profil')}}"><span class="mif-user"></span> Profil</a></li>             
+            <li><a href="{{ url('pembayaran')}}"><span class="mif-money"></span> Pembayaran</a></li>
+            <li><a href="{{ url('profil/konfirmasi')}}"><span class="mif-user-check"></span> Konfirmasi</a></li>
+            <li><a href="{{ url('profil/transaksisaya')}}"><span class="mif-list"></span> Transaksi Saya</a></li>
+            <li><a href="{{ url('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><span class="mif-exit"></span> Logout</a></li>
+            <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+              {{ csrf_field() }}
+            </form>
+          </div>
+        </ul>
+      </div>
+      @endif
     </div>
-    @endif
   </div>
-</div>
